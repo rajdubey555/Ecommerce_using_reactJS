7d1c849ad70240f556bb40c999a5d2bf405408a7
rajdubey555 <dubeyraj057@gmail.com>
2026-04-02 00:34:07 +0530

    update

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar2 from '../layout/Navbar2'
-import SearchBox from '../../components/ui/SearchBox'
+import Navbar2 from './Navbar2'
+import SearchBox from '../ui/SearchBox'
 
 
 const Navbar = () => {
